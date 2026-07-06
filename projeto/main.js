deab325b00eed9e8e3f430da67cd94cc68b450b9
felipesaliba5-design <felipesaliba5@gmail.com>
2026-07-06 15:28:40 -0300

    main teste de classe aluno e pessoa

---
+++ projeto/main.js
@@ -0,0 +1,26 @@
+const Aluno = require('./pessoas/Aluno');
+const Pessoa = require('./pessoas/Pessoa');
+
+function mostrarDados(objeto) {
+    let dados = `Nome: ${objeto.getNome() || 'Indefinido'} | E-mail: ${objeto.getEmail() || 'Inválido'}`;
+    
+    if (objeto instanceof Aluno) {
+        dados += ` | Matrícula: ${objeto.getMatricula() || 'Inválida'}`;
+    } else if (objeto instanceof Professor) {
+        dados += ` | Disciplina: ${objeto.getDisciplina() || 'Indefinida'}`;
+    }
+    
+    console.log(dados);
+}
+
+console.log("\nTeste Alunos");
+const a1 = new Aluno("João", "joao@escola.edu.br", "202512");
+const a2 = new Aluno("Maria", "maria@dominio.com", "123");
+mostrarDados(a1);
+mostrarDados(a2);
+
+console.log("\nTeste Pessoas ");
+const p1 = new Pessoa("Ronaldo", "ronaldo@empresa.com");
+const p2 = new Pessoa("Ana", "ana@gmail.net");
+mostrarDados(p1);
+mostrarDados(p2);
